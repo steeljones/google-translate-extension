680101e08d5efb6b32bba7f6eceb37c1f25c74bd
Steel Jones <steel.jones@gmail.com>
2020-09-20 19:50:17 -0400

    Add selector for languages, save to local storage

--- popup.js
+++ popup.js
@@ -1,61 +1,58 @@
-let chineseHref = 'https://translate.google.com/?hl=en&tab=TT&authuser=0#view=home&op=translate&sl=en&tl=zh-CN';
-let spanishHref = 'https://translate.google.com/?hl=en&tab=TT&authuser=0#view=home&op=translate&sl=en&tl=es';
-
-let fullText, tabId, inputEl, output, textToCopy, notificationEl;
+let fullText, inputEl, output, textToCopy, notificationEl, languagesContainer,
+    newLanguageSelect;
 
 let translationCount = 0;
 
-function main(){
-  let button = document.getElementById('submit');
-  button.addEventListener('click', handleSubmit);
+let targetLanguages;
 
+
+function init(){
+  loadTargetLanguages();
+  
   output = document.getElementById('output');
 
-  chrome.runtime.onMessage.addListener( handleMessage );
-  injectTheScript();
-
-  inputEl = document.getElementById( 'input' );
-  inputEl.addEventListener('click', handleInputFocus);
-
-  inputEl.focus();
-
+  document.getElementById( 'input' ).focus();
+  
   notificationEl = document.getElementById('notification');
+  
+  initUI();
+
+  chrome.runtime.onMessage.addListener( handleMessage );  
 }
 
 function handleMessage(request, sender, sendResponse){
+  /*
+    The translate tab sends messages containing the result of a translation to this script.
+    This is the handler for those messages
+   */
   output = document.getElementById('output');
-  
-
-  
   translationCount++;
 
-  
-  let { text } = request;
+  if( request.languageList ){
+    renderNewLanguagesList( request.languageList );
+    return
+  }
 
-  let newline = String.fromCharCode(13, 10);
+  let { text } = request;
+  //let newline = String.fromCharCode(13, 10);
   
   fullText += text + '\n\n';
   textToCopy += text + '\n\n';
   output.innerText = fullText;
-  
-  navigator.clipboard.writeText(textToCopy)
 
-  if(translationCount >= 2){
-    notificationEl.classList.remove('hidden')
-    return
+  if(translationCount == targetLanguages.length){
+    notificationEl.classList.remove('hidden');
+    navigator.clipboard.writeText(textToCopy);
   }
-  
-  setTimeout( () => {
-    let msg = {text: inputEl.value, language: 'es'}
-    chrome.tabs.sendMessage(tabId, msg);
-  }, 500 );
-
-
 }
 
-function handleSubmit(){
-
+async function handleSubmit(){
+  let inputEl = document.getElementById( 'input' );
   let text = inputEl.value;
+  if(!text.trim()) return
+
+  runCodeInClientTab( `document.querySelector('textarea').value = "${text}"` );
+  await timeout( 100 );
   
   fullText = '\n\n' + text + '\n\n';
   textToCopy = "";
@@ -63,29 +60,161 @@ function handleSubmit(){
   output.innerText = '';
   notificationEl.classList.add('hidden')
   
-  setTimeout( () => {
-    let msg = {
-      text, language: 'zh-CN', initial: true
-    }
-    chrome.tabs.sendMessage(tabId, msg);
-  }, 500 );
+  for(let l of targetLanguages){
+    await waitForTranslation(l)
+  }
 }
 
-function injectTheScript() {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    tabId = tabs[0].id
-    chrome.tabs.update({url: chineseHref})
-    setTimeout( () => {
-      chrome.tabs.executeScript({file: "main.js"});
-    }, 1000 );
+function initUI(){
+  let button = document.getElementById('submit');
+  button.addEventListener('click', handleSubmit);
+  document.getElementById('new-language').addEventListener('change', handleNewLanguage);
+}
+
+function renderTargetLanguages(  ){
+  languagesContainer = document.getElementById( 'languages' );
+  for(let l of targetLanguages){
+    appendLanguageElement(l);
+  }
+
+  languagesContainer.addEventListener('click', handleLanguagesClick );
+
+  requestNewLanguagesList();
+}
+
+function appendLanguageElement(language){
+  let languageDivTemplate = `
+    <div class="language">
+      <span>${language}</span>
+      <span class="remove-language" data-language="${language}">&times</span>
+    </div>`;
+  languagesContainer.insertAdjacentHTML('beforeend', languageDivTemplate)  
+}
+
+function requestNewLanguagesList(){
+  /*
+    Get the list of languages supported by google translate from the client tab and send back to
+    popup
+  */
+  let requestCode = `
+    languages = Array.from(document.querySelector('.language_list_tl_list').querySelectorAll('.language_list_item_language_name'))
+      .map( el => el.innerText );
+    chrome.runtime.sendMessage({ languageList: languages });
+  `;
+  runCodeInClientTab( requestCode )
+}
+
+
+function renderNewLanguagesList(languages){
+  /*
+    Render the list of languages from the google translate tab
+    */
+  newLanguageSelect = document.getElementById('new-language')
+  for( let l of languages ){
+    let optionTemplate = `
+      <option value="${l}">${l}</option>
+    `;
+    newLanguageSelect.insertAdjacentHTML('beforeend', optionTemplate);
+  }
+}
+
+function handleLanguagesClick(event){
+  if(event.target.classList.contains('remove-language')){
+    handleRemoveLanguageClick(event);
+  }
+}
+
+function handleRemoveLanguageClick(event){
+  let language = event.target.dataset.language;
+  let index = targetLanguages.indexOf( language );
+  if( index > -1 ) targetLanguages.splice( index, 1 );
+  chrome.storage.sync.set({targetLanguages}, () => {  
+    event.target.parentElement.remove();
   });
 }
 
-function handleInputFocus(){
-  let msg = {
-    url: chineseHref
-  }
-  chrome.tabs.sendMessage(tabId, msg);
+function handleNewLanguage( event ){
+  let language = event.target.value;
+  targetLanguages.push(language)
+  chrome.storage.sync.set({targetLanguages}, () => {
+    appendLanguageElement(language);
+    newLanguageSelect.value = "";
+  });
 }
 
-main();
+function loadTargetLanguages(){
+  chrome.storage.sync.get(['targetLanguages'], function(result) {
+    if(!result.targetLanguages){
+      targetLanguages = [];
+    }else{
+      targetLanguages = result.targetLanguages;
+    }
+    renderTargetLanguages( );
+  });
+}
+
+async function waitForTranslation(language){
+  /*
+    Get a translation for a given language from the google translate tab
+    */
+  openLanguagesMenu()
+  await timeout( 500 );
+  clickLanguageLink(language)
+  await timeout( 500 )
+  requestResult();
+  await timeout( 500 );
+}
+
+function clickLanguageLink(language){
+  /*
+    Click a language link on the google translate tab.
+    This selects the language as the current target for translation
+   */
+  runCodeInClientTab(
+  `languageLinks = document.querySelector('.language_list_tl_list').querySelectorAll('.language_list_item_language_name');
+  for(let n of languageLinks){
+    if(n.innerText == "${language}"){
+      n.parentElement.click();
+      break;
+    }
+  }`
+  )
+}
+
+function openLanguagesMenu(){
+  /*
+    Open the language menu in the google translate tab.  The menu needs to be open to click a language link.
+   */
+  runCodeInClientTab("document.querySelector('.tlid-open-target-language-list').click()")
+}
+
+function runCodeInClientTab(codeString){
+  /*
+    Run code in the client tab's context.  This lets you access the dom in google translate
+   */
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript({
+      code: codeString
+    });
+  });
+}
+
+function requestResult(){
+  /*
+    Copy the translation result from google translate
+   */
+  let sendMessageCode = `
+    output = document.querySelector('.tlid-translation').innerText;
+    chrome.runtime.sendMessage({ text: output });
+  `;
+  runCodeInClientTab( sendMessageCode );
+}
+
+function timeout(ms) {
+  /*
+    Helper function to await a given number of milliseconds
+   */
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+init();
